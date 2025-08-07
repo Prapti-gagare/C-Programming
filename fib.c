afe63556d4919a5efd8a4d4d6bb3ea0d89a620d4
Prapti-gagare <praptigagare11@gmail.com>
2025-08-07 08:56:10 +0530

    today

---
+++ fib.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+ {
+    int n;
+    printf("Enter the number of terms");
+    scanf("%d", &n);
+    int first = 0, second = 1, next;
+    if (n >= 1)
+        printf("%d ", first);
+    if (n >= 2)
+        printf("%d ", second);
+    for (int i = 3; i <= n; i++) {
+        next = first + second;
+        printf("%d ", next);
+        first = second;
+        second = next;
+    }
+    printf("\n");
+    return 0;
+}
