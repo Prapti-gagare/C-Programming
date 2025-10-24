d2d92656049b2ddab1be3aa48a2948b10aae0752
Prapti-gagare <praptigagare11@gmail.com>
2025-10-24 08:09:16 +0530

    Add files via upload

---
+++ search.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n, i, key, found = 0;
+    int *arr;
+
+   
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    
+    arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL) 
+    {
+        printf("Memory allocation failed!\n");
+        return 1; 
+    }
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++)
+     {
+        if (arr[i] == key) 
+        {
+            printf("Element %d found at position %d.\n", key, i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) 
+    {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    free(arr);
+
+    return 0;
+}
