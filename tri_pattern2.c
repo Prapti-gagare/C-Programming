63a8c73b7f5681283ac09810cef010e445beae10
Prapti-gagare <praptigagare11@gmail.com>
2025-10-29 08:29:24 +0530

    pattern

---
+++ tri_pattern2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, j, k;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--) 
+    {
+       
+        for (k = 0; k < n - i; k++) 
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++)
+         {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
