29c20937f1f267f6714c6d8456708e8dfac25601
Prapti-gagare <praptigagare11@gmail.com>
2025-11-04 11:39:57 +0530

    matrix vector  multiplication

---
+++ vector_multi.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+ {
+    int m, n, i, j;
+
+    printf("Enter number of rows and cols: ");
+    scanf("%d%d", &m,&n);
+
+    int matrix[m][n], vector[n], result[m];
+
+    printf("Enter elements of the %d x %d matrix:\n", m, n);
+    for (i = 0; i < m; i++) 
+    {
+        for (j = 0; j < n; j++)
+         {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Enter elements of the vector of size %d:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &vector[i]);
+    }
+
+    for (i = 0; i < m; i++) 
+    {
+        result[i] = 0;
+    }
+
+    for (i = 0; i < m; i++)
+     {
+        for (j = 0; j < n; j++) {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+
+    // Output result
+    printf("Resultant vector:\n");
+    for (i = 0; i < m; i++) {
+        printf("%d\n", result[i]);
+    }
+
+    return 0;
+}
