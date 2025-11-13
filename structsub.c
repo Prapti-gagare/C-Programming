d23eeff9b2465d00dbe0f1ee9488727db775111b
Prapti-gagare <praptigagare11@gmail.com>
2025-11-13 15:04:49 +0530

    subtraction of complex number using structure

---
+++ structsub.c
@@ -0,0 +1,22 @@
+struct complex
+{
+double real;
+double img;
+};
+int main()
+{
+    struct complex c1;
+    c1.real=10;
+    c1.img=20;
+    struct complex c2;
+    c2.real=5;
+    c2.img=2;
+
+    struct complex c3;
+    c3.real=c1.real-c2.real;
+    c3.img=c1.img-c2.img;
+
+     printf("Sum = %.2lf - %.2lfi\n", c3.real, c3.img);
+
+    return 0;
+}
