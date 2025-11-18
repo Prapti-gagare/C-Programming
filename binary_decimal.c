999622b82d80cd060a37dfe210f3fbe9c301ad2f
Prapti-gagare <praptigagare11@gmail.com>
2025-11-18 17:44:30 +0530

    decimal-binary

---
+++ binary_decimal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() 
+{
+    int decimal, quotient, remainder;
+    int binary[32];  
+    int i;
+
+    
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+
+    quotient = decimal;
+    i = 0;
+
+   
+    for (; quotient > 0; i++) 
+    {
+        remainder = quotient % 2;
+        binary[i] = remainder;
+        quotient = quotient / 2;
+    }
+
+   
+    printf("Binary equivalent: ");
+    for (int j = i - 1; j >= 0; j--) 
+    {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
