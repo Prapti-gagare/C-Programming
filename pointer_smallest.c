b148bb0749d31c7183f414f5fe6efc562fcfa2a4
Prapti-gagare <praptigagare11@gmail.com>
2025-11-30 12:40:47 +0530

    Pointer basic program

---
+++ pointer_smallest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+ {
+    int a, b;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p1 = &a; 
+    p2 = &b;   
+
+    if (*p1 < *p2) 
+    {
+        printf("smallest number = %d\n", *p1);
+    }
+     else 
+    {
+        printf("smallest number = %d\n", *p2);
+    }
+
+    return 0;
+}
