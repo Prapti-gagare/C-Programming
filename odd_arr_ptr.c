1b45f65d78f5dd808110311ef16b793e9fa34c0c
Prapti-gagare <praptigagare11@gmail.com>
2025-12-10 12:23:46 +0530

    Add files via upload

---
+++ odd_arr_ptr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int arr[100], n, i, count = 0;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    p = arr;  
+    for(i = 0; i < n; i++) 
+    {
+        if (*(p + i) % 2 == 1) 
+        {  
+            count++;
+        }
+    }
+
+    printf("Total odd numbers = %d\n", count);
+
+    return 0;
+}
