e34cf08185adc4ded1d701f67032fee2dfd5cce5
Prapti-gagare <praptigagare11@gmail.com>
2025-12-11 16:32:49 +0530

    Add files via upload

---
+++ len_ptr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    char *p;
+    int length = 0;
+
+    printf("Enter a string: ");
+    gets(str);  
+
+    p = str;     
+
+  
+    while (*p != '\0')
+     {
+        length++;
+        p++;  
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
