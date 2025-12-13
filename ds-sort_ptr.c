2833d8eedab9476fff59f6d671eec3292352708a
Prapti-gagare <praptigagare11@gmail.com>
2025-12-13 21:16:02 +0530

    Add files via upload

---
+++ ds-sort_ptr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, j, temp;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int *ptr = arr; 
+
+    printf("Enter %d numbers:\n", n);
+    for(i = 0; i < n; i++) 
+    {
+        scanf("%d", ptr + i); 
+    }
+
+   
+    for(i = 0; i < n - 1; i++)
+     {
+        for(j = i + 1; j < n; j++)
+         {
+            if(*(ptr + i) < *(ptr + j)) 
+            { 
+                temp = *(ptr + i);
+                *(ptr + i) = *(ptr + j);
+                *(ptr + j) = temp;
+            }
+        }
+    }
+
+    printf("Numbers in descending order:\n");
+    for(i = 0; i < n; i++)
+     {
+        printf("%d ", *(ptr + i));
+    printf("\n");
+     }
+    return 0;
+}
