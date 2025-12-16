b49975958cfc1cb8c672f22575547d75d253e502
Prapti-gagare <praptigagare11@gmail.com>
2025-12-16 15:57:15 +0530

    Add files via upload

---
+++ hexadecimal_decimal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+ {
+    char hex[20];
+    int decimal = 0, base = 1;
+    int i, length;
+
+    printf("Enter a hexadecimal number: ");
+    scanf("%s", hex);
+
+    length = strlen(hex);
+
+    for (i = length - 1; i >= 0; i--)
+     {
+        if (hex[i] >= '0' && hex[i] <= '9') 
+        {
+            decimal += (hex[i] - '0') * base;
+        }
+        else if (hex[i] >= 'A' && hex[i] <= 'F')
+         {
+            decimal += (hex[i] - 'A' + 10) * base;
+        }
+        else if (hex[i] >= 'a' && hex[i] <= 'f')
+         {
+            decimal += (hex[i] - 'a' + 10) * base;
+        }
+        base *= 16;
+    }
+
+    printf("Decimal equivalent = %d\n", decimal);
+
+    return 0;
+}
