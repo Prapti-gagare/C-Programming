c7065a5390760a407b8cce9c42c9b4e348bfb943
Prapti-gagare <praptigagare11@gmail.com>
2025-12-24 12:14:50 +0530

    Add files via upload

---
+++ function_cube.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int cube(int x)
+ {
+    return x * x * x;
+}
+
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Cube of %d is %d\n", num, cube(num));
+    return 0;
+}
