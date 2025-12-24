87a5035f65da9f29255df54f07834dfeaf00db45
Prapti-gagare <praptigagare11@gmail.com>
2025-12-24 12:12:58 +0530

    Add files via upload

---
+++ function_sqr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int square(int x)
+ {
+    return x * x;
+}
+
+int main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Square of %d is %d\n", num, square(num));
+    return 0;
+}
